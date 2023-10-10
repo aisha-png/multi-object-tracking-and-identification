48f4a1c3bf66f242b0481adac4338381f122f580
aisha-png <67974517+aisha-png@users.noreply.github.com>
2023-10-10 01:21:56 +0100

    video frames work, error keyboardInterupt

--- main/object_tracking.py
+++ main/object_tracking.py
@@ -1,3 +1,17 @@
 import cv2
 import numpy as np
 
+cap = cv2.VideoCapture("assets/motorway_video.mp4")
+
+while True:
+    _, frame = cap.read()
+
+    cv2.imshow("Frame", frame)
+
+    key = cv2.waitKey(1)
+    if key == 27:
+        break
+
+cap.release()
+cv2.destroyAllWindows()
+
