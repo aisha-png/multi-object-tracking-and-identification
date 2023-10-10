0918057955bf6968707d4b0ca8bdb9acace4c37c
aisha-png <67974517+aisha-png@users.noreply.github.com>
2023-10-10 17:12:55 +0100

    black and white pixels are visible

--- main/main.py
+++ main/main.py
@@ -1,3 +1,12 @@
-import cv2
+import cv2 as cv
 import numpy as np
 
+# load images
+haystack_img = cv.imread('assets/ac_pumpkins.png', cv. IMREAD_UNCHANGED)
+needle_img = cv.imread('assets/ac_single_pumpkin.png', cv.IMREAD_UNCHANGED)
+
+# call match template (cv.TM_CCOEFF_NORMED seems to give the better result) // comparison method
+result = cv.matchTemplate(haystack_img, needle_img, cv.TM_CCOEFF_NORMED)
+
+# cv.imshow('Result', result)
+# cv.waitKey()
